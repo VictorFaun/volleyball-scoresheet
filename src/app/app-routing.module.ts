633c708804925a1c0fdf1ac71854f18b855f43e7
Victor Faundez <victor.faundez@impact.cl>
2025-05-13 18:05:02 -0400

    Registrar partido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'create',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'create',
+    loadChildren: () => import('./pages/create/create.module').then( m => m.CreatePageModule)
   },
 ];
 
---
+++ src/app/pages/create/create.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.page.html',
+  styleUrls: ['./create.page.scss'],
+  standalone: false,
+})
+export class CreatePage implements OnInit {
+
+  players:any = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
