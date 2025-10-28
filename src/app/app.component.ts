b49fa33da50f3427d4f173951bbd70a68c035c60
Victor Faundez <victor.faundez.f@gmail.com>
2025-10-28 03:57:21 -0300

    mejoras tema y elementos ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,18 +9,7 @@ import { Platform } from '@ionic/angular';
 
 export class AppComponent {
 
-  constructor(private platform: Platform, private renderer: Renderer2) {
-    if ((this.platform.is('mobile') || this.platform.is('android') || this.platform.is('ios')) && !this.platform.is('mobileweb')) {
-      this.renderer.addClass(document.body, 'is-mobile');
-    }
-
-    if(this.platform.is('mobile') && this.platform.is('ios')){
-      this.renderer.addClass(document.body, 'is-ios');
-    }
-
-    if(this.platform.is('mobile') && this.platform.is('android')){
-      this.renderer.addClass(document.body, 'is-android');
-    }
+  constructor() {
   }
 
 }
